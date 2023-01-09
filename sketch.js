ecfde97b6164d5a388831425df68550f19d88834
Ibbusr <122274805+Ibbusr@users.noreply.github.com>
2023-01-09 17:12:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var arr1 = [68, 63, 23, 12,67,89,89.65,34,22]
+console.log(arr1)
+
+var total = 0
+
+for (var i = 0; i < arr1.length; i++) {
+
+    total += arr1[i]
+    console.log(total)
+}
+
+var average = total / arr1.length
